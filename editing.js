00be5969f3c595b1e330444a0e5e970c41836d85
MONICA-K-9600 <95023844+MONICA-K-9600@users.noreply.github.com>
2022-03-08 10:48:06 +0530

    Add files via upload

---
+++ editing.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+console.log(3+4);
+
+function fun2(){
+var usrname=document.getElementById("username").value;
+var usrini=document.getElementById("userinitial").value;
+var date=document.getElementById("dob").value;
+var college=document.getElementById("colname").value;
+console.log("user registered: "+usrname+" "+usrini+" ");
+console.log("dob: "+date);
+console.log("college: "+college);
+
+}
---
+++ logininfo.js
@@ -0,0 +1,7 @@
+
+function fun1(){
+var usr=document.getElementById("text1").value;
+var pwd=document.getElementById("text2").value;
+
+alert("logged in successfully : "+usr);
+console.log("Username: "+usr+ " password: " +pwd);}
